af77013a0ea56c1b86733419f34765a12cd56dfd
abiodunnoah <ajaooalsco@gmail.com>
2024-07-05 16:11:34 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <MainNav></MainNav> -->
+  <main-nav />
+</template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    // MainNav: MainNav,
+    MainNav,
+  },
+};
+</script>
